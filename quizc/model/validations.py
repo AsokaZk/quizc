c91da6e1494c3e12540efef7d3ade33b1dd73858
AsokaZk <ashok_miau@hotmail.com>
2020-09-22 17:49:14 -0400

    Validations

--- quizc/model/validations.py
+++ quizc/model/validations.py
@@ -29,6 +29,14 @@ class MinValidator(object):
             errors.append(self.MESSAGE.format(min_value=condition_value))
 
 
+class MaxValidator(object):
+    MESSAGE = "The value must be greater than {max_value}"
+
+    def validate(self, value, condition_value, errors):
+        if int(value) > condition_value:
+            errors.append(self.MESSAGE.format(max_value=condition_value))
+
+
 class MinLengthValidator(object):
     MESSAGE = "The value length must be less than {max_length}"
 
@@ -36,15 +44,29 @@ class MinLengthValidator(object):
         if len(value) < condition_value:
             errors.append(self.MESSAGE.format(max_length=condition_value))
 
+class MaxLengthValidator(object):
+    MESSAGE = "The value length must be greater than {min_length}"
 
+    def validate(self, value, condition_value, errors):
+        if len(value) > condition_value:
+            errors.append(self.MESSAGE.format(min_length=condition_value))
+
+class UpperCaseValidator(object):
+    MESSAGE = "The value must be {uppercase} only"
+
+    def validate(self, value, condition_value, errors):
+        if value.islower():
+            errors.append(self.MESSAGE.format(uppercase='uppercase'))
 
 
 class ValidatorType(Enum):
     REQUIRED = (1, RequiredValidator())
     DATE = (2, DateValidator())
     MIN = (3, MinValidator())
-    MIN_LENGTH = (4, MinLengthValidator())
-
+    MAX = (4, MaxValidator())
+    MIN_LENGTH = (5, MinLengthValidator())
+    MAX_LENGTH = (6, MaxLengthValidator())
+    UPPERCASE = (7, UpperCaseValidator())
     def __init__(self, code, validator_instance):
         self.code = code
         self.validator_instance = validator_instance
